68e4cda4af9cb50066a36c02b1d1f1c4b3f8461f
Richard Horton <ezchx@users.noreply.github.com>
2017-01-02 22:31:14 -0600

    Add files via upload

---
+++ rc_controller_esp.ino
@@ -0,0 +1,53 @@
+/*
+ *  Simple HTTP get webclient test
+ */
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "ATT9H992a4";
+const char* password = "5y3z3c7t3b9p";
+
+const char* host = "ezchx.com";
+
+void setup() {
+  Serial.begin(9600); // was 115200
+  delay(100);
+
+  // We start by connecting to a WiFi network
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  delay(5000);
+
+}
+
+
+void loop() {
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/ai_robotics/direction.php";
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(500);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('~');
+    Serial.print(line.substring(line.length()-1, line.length()));
+  }
+  
+}
